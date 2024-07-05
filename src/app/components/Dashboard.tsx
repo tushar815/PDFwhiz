2b4347b15056d11590880ebc78048726b54610ec
Tushar Kalbhor <=tskalbhor@gmail.com>
2024-07-04 21:55:10 -0600

    let user delete files

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -2,14 +2,31 @@
 
 import UploadButton from "@/app/components/UploadButton";
 import {trpc} from "@/app/_trpc/client";
-import {Ghost, MessageSquare, Plus, Trash} from "lucide-react";
+import {Ghost, Loader2, MessageSquare, Plus, Trash} from "lucide-react";
 import Link from "next/link";
 import Skeleton from "react-loading-skeleton";
 import {format} from "date-fns";
 import {Button} from "@/components/ui/button";
+import {useState} from "react";
 
 const Dashboard = () => {
+
+    const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<String | null>(null);
+
+    const utils = trpc.useContext() // forces refresh
     const {data: files, isLoading} = trpc.getUserFiles.useQuery()
+
+    const {mutate: deleteFile} = trpc.deleteFile.useMutation({
+        onSuccess: () => {
+            utils.getUserFiles.invalidate();
+        }, // on getting success from api
+        onMutate: ({id}) => {
+            setCurrentlyDeletingFile(id);
+        },    //called right away after clicking on button,
+        onSettled: () => {
+            setCurrentlyDeletingFile(null);
+        }
+    })
 
     return (
         <main className='mx-auto max-w-7xl md:p-10'>
@@ -26,15 +43,16 @@ const Dashboard = () => {
                 <ul className=' mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200
                 md:grid-cols-2 lg:grid-cols-3'>
                     {files.sort(
-                        (a,b) =>
-                        new Date(b.createdAt).getTime() -
-                        new Date(a.createdAt).getTime())
+                        (a, b) =>
+                            new Date(b.createdAt).getTime() -
+                            new Date(a.createdAt).getTime())
                         .map((file) => (
                             <li className='col-span-1 divide-y divide-gray-200 rounded-lg bg-white
                              shadow transition hover:shadow-lg' key={file.id}>
-                                <Link className='flex flex-col gap-2' href={`/dashboard/${file.id}`} >
+                                <Link className='flex flex-col gap-2' href={`/dashboard/${file.id}`}>
                                     <div className='pt-6 px-6 flex w-full items-center justify-between space-x-6'>
-                                        <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-rose-300 to to-red-500'/>
+                                        <div
+                                            className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-rose-300 to to-red-500'/>
                                         <div className='flex-1 truncate'>
                                             <div className='flex items-center space-x-3'>
                                                 <h3 className='truncate text-lg font-medium text-zinc-900'>{file.name}</h3>
@@ -42,21 +60,25 @@ const Dashboard = () => {
                                         </div>
                                     </div>
                                 </Link>
-                                <div className='px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500'>
-                                {/*Date of file*/}
-                                <div className='flex items-center gap-2'>
-                                <Plus className='h-4 w-4 '/>
-                                    {format(new Date(file.createdAt), "MMM yyyy")}
-                                </div>
+                                <div
+                                    className='px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500'>
+                                    {/*Date of file*/}
+                                    <div className='flex items-center gap-2'>
+                                        <Plus className='h-4 w-4 '/>
+                                        {format(new Date(file.createdAt), "MMM yyyy")}
+                                    </div>
                                     {/*Messages on file*/}
                                     <div className='flex items-center gap-2'>
-                                        <MessageSquare className='h-4 w-4' />
+                                        <MessageSquare className='h-4 w-4'/>
                                         mocked
                                     </div>
-                                {/*    Delete file*/}
-                                        <Button size='sm' className='w-full' variant='destructive'>
-                                            <Trash className='h-4 w-4' />
-                                        </Button>
+                                    {/*    Delete file*/}
+                                    <Button size='sm' className='w-full' variant='destructive'
+                                            onClick={() => deleteFile({id: file.id})}>
+                                        {currentlyDeletingFile === file.id ?
+                                            (<Loader2 className='h-4 w-4 animate-spin'/>) :
+                                            <Trash className='h-4 w-4'/>}
+                                    </Button>
                                 </div>
                             </li>
                         ))}
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -39,7 +39,27 @@ export const appRouter = router({
                 userId
             }
         });
-    })
+    }),
+    deleteFile: privateProcedure.input(z.object({ id: z.string()})).mutation( async ({ctx, input}) => {
+        const {userId} = ctx;
+        const file = await db.file.findFirst({
+            where:{
+                id: input.id,
+                userId,
+            }
+        });
+
+        if(!file){
+            throw  new TRPCError({code: 'NOT_FOUND'})
+        }
+        await db.file.delete({
+            where:{
+                id: input.id,
+            }
+        })
+        return file;
+    }) //inputs needs id and id should be string
+
 });
 
 // export type definition of API
