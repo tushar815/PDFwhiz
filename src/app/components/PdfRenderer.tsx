00fc780bca417e546efa3805b1f98cb613046cc9
Tushar Kalbhor <tskalbhor@gmail.com>
2024-07-12 16:30:14 -0600

    add pdf renderer

--- src/app/components/PdfRenderer.tsx
+++ src/app/components/PdfRenderer.tsx
@@ -1,13 +1,59 @@
-const PdfRenderer = () => {
-  return(
-      <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
-        <div className='h-14 w-full border-b border-r-zinc-200 flex items-center justify-between px-2'>
-          <div className='flex items-center gap-1.5'>
-            Top Bar
-          </div>
-        </div>
+"use client";
 
-      </div>
-  )
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+import { ChevronDown, Loader2 } from "lucide-react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+import { useResizeDetector } from "react-resize-detector";
+
+//pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+interface PdfRendererProps {
+  url: string;
 }
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+  const { width, ref } = useResizeDetector();
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-r-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">
+          <Button variant="ghost" aria-label="previous page">
+            <ChevronDown className="w-4 h-4" />
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex-1 w-full max-h-screen">
+        <div ref={ref}>
+          <Document
+            loading={
+              <div className="justify-center flex">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />{" "}
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: "Error loading pdf",
+                description: "Please try again later.",
+                variant: "destructive",
+              });
+            }}
+            file={url}
+            className="max-h-full"
+          >
+            <Page width={width ? width : 1} pageNumber={1} />
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default PdfRenderer;
