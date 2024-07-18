424fd6d7c2fd4e2c894d54d1321e103873605b1c
Tushar Kalbhor <tskalbhor@gmail.com>
2024-07-17 18:53:17 -0600

    add input box to chat

--- src/app/components/chat/ChatInput.tsx
+++ src/app/components/chat/ChatInput.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
-
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Send } from "lucide-react";
+import React from "react";
 
 interface ChatInputProps {
-    isDisabled?: boolean
+  isDisabled?: boolean;
 }
-const ChatInput = ({isDisabled}: ChatInputProps) => {
+const ChatInput = ({ isDisabled }: ChatInputProps) => {
   return (
-    <div>ChatInput</div>
-  )
-}
+    <div className="absolute bottom-0 left-0 w-full">
+      <form className="mx-2 flex flex-row gap-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+          <div className="relative flex flex-col w-full flex-grow p-4">
+            <div className="relative">
+              <Textarea
+                className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scsrollbar-w-2 scrolling-touch"
+                rows={1}
+                maxRows={4}
+                autoFocus
+                placeholder="Enter your Question..."
+              />
+              <Button className="absolute bottom-1.5 right-[8px]" aria-label="send message" ><Send className="h-4 w-4" /></Button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default ChatInput
+export default ChatInput;
