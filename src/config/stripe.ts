96f03ddcd0c90101a2a48f19266a3aacd5eff0b1
Tushar <tskalbhor@gmail.com>
2024-07-31 23:24:43 -0600

    add stripe and pricing

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+    {
+        name: "Free",
+        slug: "free",
+        quota: 10,
+        pagesPerPdf: 5,
+        price: {
+            amount: 0,
+            priceIds: {
+                test: '',
+                production: ''
+            }
+        },
+    },
+    {
+        name: "pro",
+        slug: "pro",
+        quota: 50,
+        pagesPerPdf: 25,
+        price: {
+            amount: 10,
+            priceIds: {
+                test: 'price_1PgfKlCK9cdBif8jXdiCNkUF',
+                production: ''
+            }
+        },
+    }
+]
